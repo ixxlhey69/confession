99993c83fc58534e94dbf9332919565c1213810f
Izdhan Imran <127573655+ixxlhey69@users.noreply.github.com>
2024-01-04 20:44:18 +0000

    Update dop.py

--- dop.py
+++ dop.py
@@ -96,4 +96,4 @@ async def request(ctx, *, code : str=None):
 #    await ctx.message.delete()
 #    await confession.delete()
 
-bot.run('MTAwMTA4MTc0NjkxMTE0MTk2OA.GBlAG9.6FGiBDDcHYSEv57LXJjfvO0XDTpYAuIO-sd9l0')
+bot.run('MTAwMTA4MTc0NjkxMTE0MTk2OA.GIAept.6-Ziv5Oe7yUjFngB3dWDRV2rqWKuQPPPcCFvH0')
